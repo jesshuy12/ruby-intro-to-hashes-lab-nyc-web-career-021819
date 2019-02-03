06562610c91e03a02f0ac2732ca7a56a3a88c146
Jessy Hu <jesshuy12@gmail.com>
2019-02-03 18:19:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ end
 def monopoly_with_second_tier
 	monopoly = {}
       monopoly = {:railroads => {
-                    :pieces => 
+                    :pieces => 4,
+                    :names => {}
         
         
       }
