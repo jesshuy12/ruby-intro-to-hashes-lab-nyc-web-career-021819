7b76533432d2e6dbadd8e9a973049ed9d6cb707f
Jessy Hu <jesshuy12@gmail.com>
2019-02-03 18:21:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,9 @@ def monopoly_with_third_tier
 	monopoly = {}
       monopoly = {:railroads => {
                     :pieces => 4,
-                    :names => {},
+                    :names => {
+                      
+                    },
                     :rent_in_dollars => {}
       }
       }
